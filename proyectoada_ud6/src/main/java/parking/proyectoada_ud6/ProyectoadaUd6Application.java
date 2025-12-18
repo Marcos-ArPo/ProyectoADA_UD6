3b3642ac90c88cbb78ed8afb453a60c5be211cdc
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-12-18 13:13:19 +0100

    Main completado

--- proyectoada_ud6/src/main/java/parking/proyectoada_ud6/ProyectoadaUd6Application.java
+++ proyectoada_ud6/src/main/java/parking/proyectoada_ud6/ProyectoadaUd6Application.java
@@ -8,6 +8,11 @@ public class ProyectoadaUd6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoadaUd6Application.class, args);
+		System.out.println("Parking API iniciada en http://localhost:8080");
+        System.out.println("Endpoints disponibles:");
+        System.out.println("  - GET    /parking/proyectoada_ud6/clientes");
+        System.out.println("  - GET    /parking/proyectoada_ud6/reservas");
+        System.out.println("  - GET    /parking/proyectoada_ud6/plazas");
 	}
 
 }
